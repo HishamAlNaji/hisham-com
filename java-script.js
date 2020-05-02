0efd872c90719a02cae29ee216ce4e7b43b2671b
HishamAl-Naji <hishamnaji1010@gmail.com>
2020-05-03 00:50:12 +0300

    ready

--- java-script.js
+++ java-script.js
@@ -14,27 +14,46 @@
 
 'use strict';
 
-function userExperience(){
+function userExperience() {
     var answer;
-    var username ='';
-    
-    var savenv = prompt("Do you want to save the enivroment(yes/no)");
-    if (savenv == "yes") {
-        answer = confirm('COOL ! NOW you should read some of our advices');
-    } else if  (savenv == "no"){
-        answer = alert('WHY !! YOU SHOULD read some of our advices to change your mind');
+    var username = '';
+    var savenv = '';
+
+    while (savenv != 'yes' && 'no') {
+        savenv = prompt("Do you want to save the enivroment(yes/no)");
+        if (savenv == "yes") {
+            answer = confirm('COOL ! NOW you should read some of our advices');
+        } else if (savenv == "no") {
+            answer = alert('WHY !! YOU SHOULD read some of our advices to change your mind');
+        }
     }
-    
-    
+
+
     if (savenv == "yes") {
-        username = prompt('please give us your name');
-    } else if ( savenv == "no") {
+        username = prompt('please give us your name', 'Guest');
+    } else if (savenv == "no") {
         username = alert('thank you for your visiting');
     }
+
+
     return username;
+
 }
 
 
+function userPrize() {
+
+    var stars = prompt("how many stars do you want to achieve in this session by reading instructions?")
+    var prize = 0 ;
+
+    for (var i = 0; i < stars; i++) {
+
+        prize = prize + 1 ;
+    }
+
+    return prize ;
+
+}
 
 
 
